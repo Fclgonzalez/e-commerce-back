2a9f3e9bebf66b8497afc306e0fce73b972f7619
Lucas Oliveira <lucas.oliveirab0531@gmail.com>
2022-08-03 11:52:24 -0300

    primeiro commit

---
+++ src/main/java/com/ecommerce/imobiliaria/ImobiliariaApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.imobiliaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImobiliariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImobiliariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/imobiliaria/ImobiliariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.imobiliaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImobiliariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
