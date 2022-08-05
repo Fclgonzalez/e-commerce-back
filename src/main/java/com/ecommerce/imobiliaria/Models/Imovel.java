f59544c48629a94c4469e88c5abd2b579b822b99
MarcosASampaio <marcosgamer@hotmail.com>
2022-08-05 11:23:39 -0300

    criação do repository e model do imovel

--- src/main/java/com/ecommerce/imobiliaria/Models/Imovel.java
+++ src/main/java/com/ecommerce/imobiliaria/Models/Imovel.java
@@ -1,14 +1,55 @@
 package com.ecommerce.imobiliaria.Models;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import com.ecommerce.imobiliaria.Models.Enums.FinalidadeImovel;
+import com.ecommerce.imobiliaria.Models.Enums.TipoImovel;
+import org.springframework.format.annotation.NumberFormat;
 
+import javax.persistence.*;
+
+@Entity
 public class Imovel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer idEndereco;
+    private Integer idImovel;
 
+    @NumberFormat(pattern = "#.##0,00")
+    @Column(nullable = false)
+    private double dataCriacao;
 
+    @Column
+    private Boolean contratoAluguel = false;
+
+    @Column
+    private Boolean contratoVenda = false;
+
+    @Column(nullable = false)
+    private double valorAluguel;
+
+    @Column(nullable = false)
+    private double valorVenda;
+
+    @Column(nullable = false)
+    private double area;
+
+    @Column(nullable = true)
+    private String descricao;
+
+    @Column
+    private Integer quartos;
+
+    @Column
+    private Integer suite;
+
+    @Column
+    private Integer banheiros;
+
+    @Column
+    private Integer vagas;
+
+    @Enumerated(EnumType.STRING)
+    private FinalidadeImovel finalidadeImovel;
+
+    @Enumerated(EnumType.STRING)
+    private TipoImovel tipoImovel;
 }
